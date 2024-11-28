94b8f5e9daa05bd65193154f00145082d84d76aa
Ellen Lee <kwellen@umich.edu>
2024-11-27 21:25:34 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-function openLightbox1() {
-    const lightbox = document.getElementById("lightbox_monster");
-    lightbox.style.display = "block";
-    const captionText = document.getElementById("caption");
-    captionText.innerHTML = "Who is the monster?";
-}
+// function openLightbox1() {
+//     const lightbox = document.getElementById("lightbox_monster");
+//     lightbox.style.display = "block";
+//     const captionText = document.getElementById("caption");
+//     captionText.innerHTML = "Who is the monster?";
+// }
 
 function closeLightbox() {
     const lightbox = document.getElementById("lightbox_monster");
     lightbox.style.display = "none";
 }
+
+document.querySelector("#monster_img").addEventListener("click", function(){
+    const lightbox = document.getElementById("lightbox_monster");
+    lightbox.style.display = "block";
+    const captionText = document.getElementById("caption");
+    captionText.innerHTML = "Who is the monster?";
+});
